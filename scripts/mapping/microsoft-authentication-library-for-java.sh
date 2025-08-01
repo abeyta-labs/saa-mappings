dc68ef952ce2acda29c309f999e84b881910abb1
Robert Abeyta <robert.abeyta@broadcom.com>
2025-08-01 09:12:16 -0700

    adding jolokia

--- scripts/mapping/microsoft-authentication-library-for-java.sh
+++ scripts/mapping/microsoft-authentication-library-for-java.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-advisor mapping build -r https://github.com/AzureAD/microsoft-authentication-library-for-java -c com.microsoft.azure:msal4j
+/tmp/advisor mapping build -r https://github.com/AzureAD/microsoft-authentication-library-for-java -c com.microsoft.azure:msal4j
